861132bea9e59b0740fd0db1fc2391a4a9f7e5ec
vadimpk <vadyman.pk@gmail.com>
2020-11-29 21:48:38 +0200

    added .gitignore

--- compile_data/src/connect_db.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-import ssl
-
-client = MongoClient("mongodb+srv://vadimpk:lz921skm0001p@cluster0-bawxm.mongodb.net/cr_stats?retryWrites=true&w=majority", ssl_cert_reqs=ssl.CERT_NONE)
-
-db = client["cr_stats"]
-
--- server/connect_db.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://vadimpk:lz921skm0001p@cluster0-bawxm.mongodb.net/cr_stats?retryWrites=true&w=majority")
-
-db = client["cr_stats"]
-
